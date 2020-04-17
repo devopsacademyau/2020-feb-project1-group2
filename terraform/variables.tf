f252e94d219ebd155ea0c9c2eb9a1ddd69aded24
deniseddi <45111486+deniseddi@users.noreply.github.com>
2020-04-17 13:55:01 +1000

    Update variables.tf
    Added the /24 cidr to each of the subnets, instead of /18.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -68,21 +68,21 @@ variable "cidr_vpc" {
 
 variable "private_subnet-wp-a" {
     type    = string
-    default = "10.0.63.0/18"
+    default = "10.0.63.0/24"
 }
 
 variable "private_subnet-wp-b" {
     type    = string
-    default = "10.0.127.0/18"
+    default = "10.0.127.0/24"
 }
 
 variable "public_subnet-wp-a" {
     type    =  string
-    default = "10.0.191.0/18"
+    default = "10.0.191.0/24"
 }
 
 variable "public_subnet-wp-b" {
     type    =  string
-    default = "10.0.255.0/18"
+    default = "10.0.255.0/24"
 }
 
