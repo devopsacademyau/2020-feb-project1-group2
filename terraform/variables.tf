7676ef4c3a70149621b700fd7d4aa97f17d05dd0
drii-cavalcanti <drii.cavalcanti@gmail.com>
2020-04-19 23:26:10 +1000

    temp ecs file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,56 +1,7 @@
-variable "additional_user_data_script" {
-  description = "Additional user data script (default=\"\")"
-  default     = ""
-}
-
-variable "asg_max_size" {
-  description = "Maximum number EC2 instances"
-  default     = 2
-}
-
-variable "asg_min_size" {
-  description = "Minimum number of instances"
-  default     = 1
-}
-
-variable "asg_desired_size" {
-  description = "Desired number of instances"
-  default     = 2
-}
-
-variable "image_id" {
-  description = "AMI image_id for ECS instance"
-  default     = "ami-064db566f79006111"
-}
-
-variable "instance_keypair" {
-  description = "Instance keypair name"
-  default     = "starkmatt"
-}
-
-variable "instance_log_group" {
-  description = "Instance log group in CloudWatch Logs"
-  default     = ""
-}
-
-variable "instance_root_volume_size" {
-  description = "Root volume size (default=50)"
-  default     = 50
-}
-
-variable "instance_type" {
-  description = "EC2 instance type (default=t2.micro)"
-  default     = "t2.micro"
-}
 
 variable "name" {
   description = "Base name to use for resources in the module"
   default     = "da-wordpress"
-}
-
-variable "tags" {
-  description = "A map of tags to add to all resources"
-  default     = {}
 }
 
 variable "vpc_id" {
@@ -88,3 +39,21 @@ variable "public_subnet-wp-b" {
   default = "10.0.255.0/24"
 }
 
+variable "ecr_repository-image" {
+  type    = string
+  default = "wp-image"
+}
+
+# the ECS optimized AMI's change by region. You can lookup the AMI here:
+# https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html
+variable "image_id" {
+  type        = string
+  description = "AMI image_id for ECS instance"
+  default     = "ami-064db566f79006111"
+}
+
+variable  "instance_type" {
+    type        = string
+    description = "AMI instance_type for ECS instance"
+    default     = "t2.micro"
+}
