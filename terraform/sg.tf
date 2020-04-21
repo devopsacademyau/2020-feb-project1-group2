e7076f99719b372e0be6d7b5266fad4fb7adc2c8
Matt <48781706+starkmatt@users.noreply.github.com>
2020-04-21 20:55:45 +1000

    Update sg.tf

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -1,26 +1,3 @@
-resource "aws_security_group" "wordpress-access" {
-    name = "wordpress-access"
-    description = "Allow inbound traffic to Wordpress Instance"
-    vpc_id = "${aws_vpc.da-wordpress-vpc.id}"
-
-    tags = {
-        Name = "wordpress-access"
-    }
-    ingress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-}
-
 resource "aws_security_group" "database" {
     name = "database"
     description = "Allow inbound traffic"
