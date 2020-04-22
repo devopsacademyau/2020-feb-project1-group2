343048cf71fdc0d8f8480daf1cc93264060e652b
deniseddi <45111486+deniseddi@users.noreply.github.com>
2020-04-22 14:47:43 +1000

    Updated parameter_store.tf
    Fixed minor issues on "description"

--- terraform/parameter_store.tf
+++ terraform/parameter_store.tf
@@ -1,27 +1,27 @@
 resource "aws_ssm_parameter" "wordpress-db-host" {
   name        = "/wordpress/WORDPRESS_DB_HOST"
-  description = "The host parameter to be used by the container and DB"
+  description = "The host parameter to be used by the container"
   type        = "SecureString"
   value       = "secret"
 }
 
 resource "aws_ssm_parameter" "wordpress-db-user" {
   name        = "/wordpress/WORDPRESS_DB_USER"
-  description = "The user parameter to be used by the container and DB"
+  description = "The user parameter to be used by the container"
   type        = "SecureString"
   value       = "secret"
 }
 
 resource "aws_ssm_parameter" "wordpress-db-password" {
   name        = "/wordpress/WORDPRESS_DB_PASSWORD"
-  description = "The password parameter to be used by the container and DB"
+  description = "The password parameter to be used by the container"
   type        = "SecureString"
   value       = "secret"
 }
 
 resource "aws_ssm_parameter" "wordpress-db-name" {
   name        = "/wordpress/WORDPRESS_DB_NAME"
-  description = "The name parameter to be used by the container and DB"
+  description = "The name parameter to be used by the container"
   type        = "SecureString"
   value       = "secret"
 }
