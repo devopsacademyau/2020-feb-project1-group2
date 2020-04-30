7b251f973bdcb571215d74ccfffcd898c0dd6b89
deniseh <deniseddi@hotmail.com>
2020-04-30 20:27:13 +1000

    added random password creation

--- terraform/parameter_store.tf
+++ terraform/parameter_store.tf
@@ -2,26 +2,34 @@ resource "aws_ssm_parameter" "wordpress-db-host" {
   name        = "/wordpress/WORDPRESS_DB_HOST"
   description = "The host parameter to be used by the container and DB"
   type        = "SecureString"
-  value       = "secret"
+  value       = random_password.value.result
 }
 
 resource "aws_ssm_parameter" "wordpress-db-user" {
   name        = "/wordpress/WORDPRESS_DB_USER"
   description = "The user parameter to be used by the container and DB"
   type        = "SecureString"
-  value       = "secret"
+  value       = random_password.value.result
 }
 
 resource "aws_ssm_parameter" "wordpress-db-password" {
   name        = "/wordpress/WORDPRESS_DB_PASSWORD"
   description = "The password parameter to be used by the container and DB"
   type        = "SecureString"
-  value       = "secret"
+  value       = random_password.value.result
 }
 
 resource "aws_ssm_parameter" "wordpress-db-name" {
   name        = "/wordpress/WORDPRESS_DB_NAME"
   description = "The name parameter to be used by the container and DB"
   type        = "SecureString"
-  value       = "secret"
+  value       = random_password.value.result
 }
+
+resource "random_password" "value" {
+  length           = 10
+  special          = true
+  override_special = "_%@"
+}
+
+
