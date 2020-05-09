4f3480d2ff213aa274cb005039779add4c5e8692
Matt G <mattgar92@hotmail.com>
2020-05-09 08:05:51 +0000

    Old IAM...

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -35,3 +35,38 @@ resource "aws_iam_role_policy_attachment" "ecs-service-attach-c" {
   role       = "${aws_iam_role.ecs-instance-role.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+
+
+
+resource "aws_iam_role" "ecs-role" {
+  name = "${var.project_name}-ecs"
+  assume_role_policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+  {
+    "Effect": "Allow",
+    "Principal": {
+      "Service": "ecs-tasks.amazonaws.com"
+	    },
+    "Action": "sts:AssumeRole"
+  }
+]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-attach-a" {
+  role       = "${aws_iam_role.ecs-role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+}
+resource "aws_iam_role_policy_attachment" "ecs-attach-b" {
+  role       = "${aws_iam_role.ecs-role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+resource "aws_iam_role_policy_attachment" "ecs-attach-c" {
+  role       = "${aws_iam_role.ecs-role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
